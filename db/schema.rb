04003ebdf26922d2a8b4060ae5a631ab84198c75
Wendy Sanchez-Vaynshteyn <WendyGSV@gmail.com>
2014-10-30 15:36:31 -0400

    added nickname to User schema and added it to user account spec test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027214202) do
+ActiveRecord::Schema.define(version: 20141030184827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141027214202) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "nickname",                            null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
